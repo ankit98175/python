700431e51623f92cef13af9b5390b25c4356d68b
Ankit Jindal <jindal8901@gmail.com>
2026-01-14 15:41:18 +0530

    Add files via upload

---
+++ assignment - 1.py
@@ -0,0 +1,21 @@
+# --------------------------Task - 1 ---------------------------
+num1 = int(input("Enter the First number: "))
+num2 = int(input("Enter the Second number: "))
+print("---------------------------")
+# Addition
+add = num1 + num2
+print("Addition of", num1, "and", num2, "is:", add)
+sub = num1 - num2
+print("Subtraction of", num1, "and", num2, "is:", sub)
+mul = num1 * num2
+print("Multiplication of", num1, "and", num2, "is:", mul)
+div = num1 / num2
+print("Division of", num1, "and", num2, "is:", div)
+
+
+# ----------------------Task - 2 ---------------------------
+
+First_name = input("Enter your First Name: ")
+Last_name = input("Enter your Last Name: ")
+Full_name = First_name + " " + Last_name
+print("Hello " + Full_name + ", have a good day!")
